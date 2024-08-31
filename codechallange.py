e131e58132558705cd2fe995aecc23ffcfd3cad8
Namada Junior <namada871@gmail.com>
2024-08-31 21:21:06 +0300

    Complete tasks and adds comments

--- codechallange.py
+++ codechallange.py
@@ -13,7 +13,7 @@ def reverse_string(text):
     print (text[::-1])
 reverse_string("maN")
 
-# Count the nimber of vowels in a word
+# Count the number of vowels in a word
 def count_vowels (text):
     vowels = "aeiou"
     count = 0
@@ -34,5 +34,50 @@ def calculate_factorial(n):
     print (f"Factotial of {n} = {factorial}")
 calculate_factorial(5)
 
+# 
+def apply_decorator(func):
+    def decorator_func(*args, **kwargs):
+        print("Decorator Applied")
+        return func(*args, **kwargs)
+    return decorator_func
 
+@apply_decorator
+def sample_function():
+    print("Original Function Called")
 
+sample_function()
+
+# Sorts people by age in ascending order
+def sort_by_age(people):
+    return sorted(people, key=lambda person: person[1])
+people = [("Nam", 30), ("Juju", 20), ("Ben", 45), ("Bob", 15)]
+sorted_people = sort_by_age(people)
+print(sorted_people)
+
+# Mergers two dictionaries
+def merge_dicts(dict1, dict2):
+    merged_dict = dict1.copy()  
+    for key, value in dict2.items():
+        if key in merged_dict:
+            merged_dict[key] += value 
+        else:
+            merged_dict[key] = value  
+    return merged_dict
+
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'b': 3, 'c': 4, 'd': 5}
+
+merged = merge_dicts(dict1, dict2)
+print(merged)
+
+# DIsplays car info
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print(f"This is a {self.make} {self.model} series, model {self.year}")
+my_car = Car("Mercedes", "SLK-200", 2016)
+my_car.display_info()
