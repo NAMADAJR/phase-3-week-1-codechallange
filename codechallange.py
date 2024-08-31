d9f9a43943902fff5fddbd33d5a89a0c71343f12
Namada Junior <namada871@gmail.com>
2024-08-31 12:06:22 +0300

    Completes 'calculate_faftorial' task

--- codechallange.py
+++ codechallange.py
@@ -1,15 +1,19 @@
+# Adds two numbers together
 def add_number (num1 , num2):
     print (num1 + num2)
 add_number(5 , 5)
 
+# Checks if a number is even
 def is_even (number):
     print (number % 2 == 0) 
 is_even(2)
 
+# Reverses the order of a string
 def reverse_string(text):
     print (text[::-1])
 reverse_string("maN")
 
+# Count the nimber of vowels in a word
 def count_vowels (text):
     vowels = "aeiou"
     count = 0
@@ -18,4 +22,17 @@ def count_vowels (text):
            count += 1
     print(f"Number of vowels: {count}")
 count_vowels("Mangoes")
+
+# Rerutns the factorial of the number 
+def calculate_factorial(n):
+    if n < 0:
+        print("Enter a non-negative number")
+        return None
+    factorial = 1
+    for i in range (2, n+1):
+        factorial *=i
+    print (f"Factotial of {n} = {factorial}")
+calculate_factorial(5)
+
+
 
